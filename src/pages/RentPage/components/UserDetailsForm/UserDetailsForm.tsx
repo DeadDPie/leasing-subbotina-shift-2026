a0377e447a0ed00e42fd74ae3775e0217a2ad2ea
Subbotina Olesya <elizabethfox434@gmail.com>
2026-01-29 18:31:00 +0700

    feat: add RentConditionsForm

--- src/pages/RentPage/components/UserDetailsForm/UserDetailsForm.tsx
+++ src/pages/RentPage/components/UserDetailsForm/UserDetailsForm.tsx
@@ -1,7 +1,12 @@
+import { useRent } from "../../contexts/RentContext";
+
 export const UserDetailsForm = () => {
+	const { data } = useRent();
+	console.log(data);
+
 	return (
 		<div>
-			<h2>UserDetailsForm заказа</h2>
+			<h2> UserDetailsForm заказа</h2>
 		</div>
 	);
 };
